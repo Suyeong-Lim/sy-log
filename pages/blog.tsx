480d13380f7f4dadb0c6b48a5bf379b7ca6528f2
Suyeong-Lim <tntn393@gmail.com>
2022-06-29 19:50:24 +0900

    :sparkles: blogPost component 추가

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,7 +1,11 @@
+import BlogPost from "../components/BlogPost";
 import Container from "../components/Container";
 const Blog = () => {
   return (
     <Container>
+      <div className={`mt-10 flex flex-col`}>
+        <BlogPost />
+      </div>
       <span className={`font-bold`}>BLOG</span>
     </Container>
   );
