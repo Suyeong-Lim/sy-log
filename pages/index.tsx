e673cb2f995cc64b24f4c5153228ffcd1a523677
Suyeong-Lim <tntn393@gmail.com>
2022-06-29 19:47:43 +0900

    :lipstick: home UI style update

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,31 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Container from "../components/Container";
+import RecentPosts from "../components/RecentPosts";
+import metadata from "../data/metadata";
 
 const Home: NextPage = () => {
   return (
     <Container>
-      <span className={`text-green-500`}>Home</span>
+      <div className={`my-5 w-full`}>
+        <div className={`relative`}>
+          <Image
+            src={`/home.png`}
+            alt="대표 이미지"
+            width={`100%`}
+            height={45}
+            layout={`responsive`}
+            objectFit="cover"
+            className={`rounded-3xl`}
+          />
+          <span
+            className={`absolute top-12 font-extrabold italic text-white text-5xl md:text-9xl text flex justify-center w-full drop-shadow-lg`}
+          >
+            {metadata.title}
+          </span>
+        </div>
+        <RecentPosts />
+      </div>
     </Container>
   );
 };
