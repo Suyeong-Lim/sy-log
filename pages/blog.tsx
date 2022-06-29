12188a4aa33a6da262490647b497d34d0cae8714
Suyeong-Lim <tntn393@gmail.com>
2022-06-29 19:36:32 +0900

    :sparkles: index page

---
+++ pages/blog.tsx
@@ -0,0 +1,10 @@
+import Container from "../components/Container";
+const Blog = () => {
+  return (
+    <Container>
+      <span className={`font-bold`}>BLOG</span>
+    </Container>
+  );
+};
+
+export default Blog;
