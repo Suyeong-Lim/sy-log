6f4cb712e0bc5c4f1694e0587b10895b14a40489
Suyeong-Lim <tntn393@gmail.com>
2022-06-29 20:21:03 +0900

    :art: blogPost props 추가

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -1,14 +1,36 @@
 import BlogPost from "../components/BlogPost";
 import Container from "../components/Container";
-const Blog = () => {
+import { allPosts } from "contentlayer/generated";
+import { InferGetStaticPropsType } from "next";
+
+const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <Container>
       <div className={`mt-10 flex flex-col`}>
-        <BlogPost />
+        {posts.map((post) => (
+          <BlogPost
+            date={post.date}
+            title={post.title}
+            des={post.description}
+            slug={post._raw.flattenedPath}
+            key={post._id}
+          />
+        ))}
       </div>
-      <span className={`font-bold`}>BLOG</span>
     </Container>
   );
+};
+
+export const getStaticProps = async () => {
+  const posts = allPosts.sort(
+    (a, b) => Number(new Date(b.date)) - Number(new Date(a.date))
+  );
+
+  return {
+    props: {
+      posts,
+    },
+  };
 };
 
 export default Blog;
